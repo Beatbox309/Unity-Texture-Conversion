9cb76b6e9cbf44c90bdfa7dc0797c493fba5a0ef
Julian Delsi <29632054+Parzival-3141@users.noreply.github.com>
2020-10-26 13:13:33 -0400

    Update BGRA_Normal_To_RGBA_Normal.py

--- TextureConversion/Scripts/BGRA_Normal_To_RGBA_Normal.py
+++ TextureConversion/Scripts/BGRA_Normal_To_RGBA_Normal.py
@@ -1,36 +1,23 @@
 from PIL import Image
 import pathlib
-import os
+import sys
 
-def SplitImg(_img):
-    if(_img.mode == "RGBA"):
-        _R,_G,_B,_A = _img.split()
-        return (_R,_G,_B,_A)
-    elif(_img.mode == "RGB"):
-        _R,_G,_B = _img.split()
-        return (_R,_G,_B)
-    elif(_img.mode == "L"):
-        _L = _img.split()
-        return (_L)
-    elif(_img.mode == "I"):
-        cImg = _img.convert("L")
-        _I = cImg.split()
-        return (_I)
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
+import TextureConversionMain as tcm
 
-def CreateBWImg(_size,_col):
-    _newImg = Image.new("L",_size,color=_col)
-    return _newImg
 
 def Convert(normPath):
     # setup
     ogNorm = Image.open(normPath)
-    normTuple = SplitImg(ogNorm)
+    normTuple = tcm.SplitImg(ogNorm)
+    print("Images Loaded")
 
     # Normal Map
-    nrmWhite = CreateBWImg(ogNorm.size,255)
+    nrmWhite = tcm.CreateBWImg(ogNorm.size,255)
     
     normal = Image.merge("RGBA",(normTuple[3],normTuple[1],nrmWhite,nrmWhite))
     normal.save(workingDir + "NormalMap.png")
+    print("Images Saved!")
 
 
 ogNormalPath = ""
@@ -44,6 +31,4 @@ ogNormalPath = ogNormalPath.rstrip("\n")
 workingDir = str(pathlib.Path(ogNormalPath).parent.absolute()) + '\\'
 Convert(ogNormalPath)
 
-os.remove(str(pathlib.Path(__file__).parent.absolute()) + '\\' + "Temp.txt")
-
-
+tcm.RemoveTempFiles()
