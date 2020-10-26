99e969fc5bb2907225674745be5af159a83209cc
Julian Delsi <29632054+Parzival-3141@users.noreply.github.com>
2020-10-26 13:14:17 -0400

    Update InvertYellowNormal.py

--- TextureConversion/Scripts/InvertYellowNormal.py
+++ TextureConversion/Scripts/InvertYellowNormal.py
@@ -1,41 +1,28 @@
 from PIL import Image
 from PIL import ImageChops
 import pathlib
-import os
+import sys
 
-def SplitImg(_img):
-    if(_img.mode == "RGBA"):
-        _R,_G,_B,_A = _img.split()
-        return (_R,_G,_B,_A)
-    elif(_img.mode == "RGB"):
-        _R,_G,_B = _img.split()
-        return (_R,_G,_B)
-    elif(_img.mode == "L"):
-        _L = _img.split()
-        return (_L)
-    elif(_img.mode == "I"):
-        cImg = _img.convert("L")
-        _I = cImg.split()
-        return (_I)
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
+import TextureConversionMain as tcm
 
-def CreateBWImg(_size,_col):
-    _newImg = Image.new("L",_size,color=_col)
-    return _newImg
 
 def Convert(normPath):
     # setup
     ogNorm = Image.open(normPath)
-    normTuple = SplitImg(ogNorm)
+    normTuple = tcm.SplitImg(ogNorm)
+    print("Images Loaded")
 
     # Invert Blue Channel
     invB = ImageChops.invert(normTuple[2])
     normTuple = (normTuple[0],normTuple[1],invB)
-    
+    print("Image Inverted")
     
     # Normal Map
-    nrmWhite = CreateBWImg(ogNorm.size,255)
+    nrmWhite = tcm.CreateBWImg(ogNorm.size,255)
     normal = Image.merge("RGBA",(normTuple[0],normTuple[1],normTuple[2],nrmWhite))
     normal.save(workingDir + "NormalMap.png")
+    print("Images Saved!")
 
 
 ogNormalPath = ""
@@ -49,6 +36,4 @@ ogNormalPath = ogNormalPath.rstrip("\n")
 workingDir = str(pathlib.Path(ogNormalPath).parent.absolute()) + '\\'
 Convert(ogNormalPath)
 
-os.remove(str(pathlib.Path(__file__).parent.absolute()) + '\\' + "Temp.txt")
-
-
+tcm.RemoveTempFiles()
