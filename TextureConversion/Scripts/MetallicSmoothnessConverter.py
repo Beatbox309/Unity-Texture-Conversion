1c24f5760d6f949aca34e1ceb47632ad589d57c8
Julian Delsi <29632054+Parzival-3141@users.noreply.github.com>
2020-10-26 13:15:18 -0400

    Update MetallicSmoothnessConverter.py

--- TextureConversion/Scripts/MetallicSmoothnessConverter.py
+++ TextureConversion/Scripts/MetallicSmoothnessConverter.py
@@ -1,39 +1,31 @@
 from PIL import Image
 from PIL import ImageChops
 import pathlib
-import os
+import sys
 
-def SplitImg(_img):
-    if(_img.mode == "RGBA"):
-        _R,_G,_B,_A = _img.split()
-        return (_R,_G,_B)
-    elif(_img.mode == "RGB"):
-        _R,_G,_B = _img.split()
-        return (_R,_G,_B)
-    elif(_img.mode == "L"):
-        _L = _img.split()
-        return (_L)
-    elif(_img.mode == "I"):
-        cImg = _img.convert("L")
-        _I = cImg.split()
-        return (_I)
+sys.path.append(str(pathlib.Path(__file__).parent.absolute()))
+import TextureConversionMain as tcm
+
 
 def Convert(metalPath, roughPath):
     # setup
     ogMetallic = Image.open(metalPath)
     ogRough = Image.open(roughPath)
     
-    metalTuple = SplitImg(ogMetallic)
-    roughTuple = SplitImg(ogRough)
+    metalTuple = tcm.SplitImg(ogMetallic)
+    roughTuple = tcm.SplitImg(ogRough)
+    print("Images Loaded")
 
     # Invert Roughness
     smoothness = ImageChops.invert(roughTuple[0])
+    print("Image Inverted")
 
     # Metallic Map
     bandsTuple = (metalTuple[0],metalTuple[1],metalTuple[2],smoothness)
     
     metallic = Image.merge("RGBA",bandsTuple)
     metallic.save(workingDir + "MetallicSmoothnessMap.png")
+    print("Images Saved!")
 
 
 # Get paths from file
@@ -51,4 +43,4 @@ ogMetallicPath = ogMetallicPath.rstrip("\n")
 workingDir = str(pathlib.Path(ogMetallicPath).parent.absolute()) + '\\'
 Convert(ogMetallicPath, ogRoughPath)
 
-os.remove(str(pathlib.Path(__file__).parent.absolute()) + '\\' + "Temp.txt")
+tcm.RemoveTempFiles()
